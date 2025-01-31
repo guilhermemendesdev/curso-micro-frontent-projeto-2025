bbb8c67c3fd242a085676b156d335c21909ac051
Guilherme <guilhermemendesdev@gmail.com>
2025-01-31 06:18:24 -0300

    feat: adiciona classname aleatorio no container

--- mfp/container/src/App.js
+++ mfp/container/src/App.js
@@ -2,16 +2,21 @@ import React from "react";
 import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 
 export default () => {
     return (
-    <div>
-        <BrowserRouter>
-            <Header>
-                <hr/>
-                <MarketingApp />
-            </Header>
-        </BrowserRouter>
-    </div>
+    <BrowserRouter>
+        <StylesProvider>
+            <div>
+                <Header/>
+                <MarketingApp />         
+            </div>
+        </StylesProvider>
+    </BrowserRouter>
     )
 }
