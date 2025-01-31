13d67f4cff27e5b97226d20a3dfed6ac8c2dd930
Guilherme <guilhermemendesdev@gmail.com>
2025-01-31 06:29:07 -0300

    feat: altera url para busca de imagens aleatorias

--- mfp/marketing/src/components/Landing.js
+++ mfp/marketing/src/components/Landing.js
@@ -114,12 +114,12 @@ export default function Album() {
         <Container className={classes.cardGrid} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
+            {cards.map((idx, card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
+                    image={`https://picsum.photos/200/300?random=${idx}`}
                     title="Image title"
                   />
                   <CardContent className={classes.cardContent}>
