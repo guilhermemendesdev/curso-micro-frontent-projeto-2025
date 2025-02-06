64388a0a26b0030f449c9096c932ac861c3df08f
Guilherme <guilhermemendesdev@gmail.com>
2025-02-06 05:57:16 -0300

    feat: configuração inicial do webpack dev, utilizando vue.js

--- mfp/dashboard/config/webpack.dev.js
+++ mfp/dashboard/config/webpack.dev.js
@@ -1,30 +1,36 @@
 const { merge } = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json')
+const packageJson = require('../package.json');
 
 const devConfig = {
-    mode: 'development',
-    devServer: {
-        port: 8082,
-        historyApiFallback: {
-            index: 'index.html'
-        },
+  mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8083/',
+  },
+  devServer: {
+    port: 8083,
+    historyApiFallback: {
+      historyApiFallback: true,
     },
-    plugins: [
-        new ModuleFederationPlugin({
-            name: 'dashboard',
-            filename: 'remoteEntry.js',
-            exposes: {
-                './DashboardApp': './src/bootstrap'
-            },
-            shared: packageJson.dependencies
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-        })
-    ],
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'dashboard',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './DashboardApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
 
-module.exports = merge(commonConfig, devConfig)
+module.exports = merge(commonConfig, devConfig);
